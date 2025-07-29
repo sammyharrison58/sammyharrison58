50c30184268122e6cebee99a94de958c82837c8d
sammyharrison58 <sammyharrison58@gmail.com>
2025-07-29 13:37:31 +0300

    apply changes

--- prac.py
+++ prac.py
@@ -9,3 +9,6 @@ total_cost = price * Quantity
 print(f"hello {name}, welcome to the system")
 print(f"you are {age} years old")
 print(f"the total cost of {item} is {total_cost}")
+#descrobing a comment
+# this is a comment
+print("nice having you here, welcome back {name}")
