acf05890eb530149cc9598b9178417d95cdba854
sammyharrison58 <sammyharrison58@gmail.com>
2025-09-16 11:11:58 +0300

    apply changes

---
+++ empoyee.py
@@ -0,0 +1,5 @@
+employee_name =input("please enter employee name")
+Departmenr = input("please enter your department")
+salary = int(input("please enter your salary"))
+new_salary = salary + 1000
+print(new_salary)
